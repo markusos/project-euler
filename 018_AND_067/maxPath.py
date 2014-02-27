b57cff7e07750dc355d209df206e112919fbc058
markusos <markus.ostberg@gmail.com>
2014-02-27 21:30:22 +0100

    Solved problem 18 and 67

---
+++ 018_AND_067/maxPath.py
@@ -0,0 +1,29 @@
+
+def maxPath(fileName):
+    triangle =[]
+    f = open(fileName, 'r')
+    for line in f:
+        triangle.append(map(int, line.replace('\n', '').split(" ")))
+
+    length = len(triangle)
+    a = []
+    for i in range(0, length):
+        a.append(triangle[i] + [0 for x in range(0,length - len(triangle[i]))])
+
+    b = [[0 for x in range(0,length)] for y in range(0,length)]
+    
+    for i in range(0, length):
+        for j in range(0, i+1):
+            if i == 0:
+                b[i][j] = a[i][j]
+            elif j == 0:
+                b[i][j] = a[i][j] + b[i-1][j]
+            else:
+                b[i][j] = a[i][j] + max(b[i-1][j], b[i-1][j-1])
+    maxVal = 0
+    
+    for j in range(0, length):
+        if maxVal < b[length-1][j]:
+            maxVal = b[length-1][j]
+
+    return maxVal
---
+++ 018_AND_067/test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+import maxPath 
+import unittest
+
+class TestMaxPath (unittest.TestCase):
+
+    def test_maxPathSum_0(self):
+        result = maxPath.maxPath("triangle_small.txt")
+        self.assertEqual(result, 23)
+    
+    def test_maxPathSum_1(self):
+        result = maxPath.maxPath("triangle.txt")
+        self.assertEqual(result, 1074)
+        print "Result: {0}".format(result)
+
+    def test_maxPathSum_2(self):
+        result = maxPath.maxPath("triangle_big.txt")
+        self.assertEqual(result, 7273)
+        print "Result: {0}".format(result)
+
+if __name__ == '__main__':
+    unittest.main()
