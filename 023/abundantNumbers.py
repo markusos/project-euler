b3aedec78ebd7ea4683240273316d791bc4124e9
markusos <markus.ostberg@gmail.com>
2014-03-03 21:22:33 +0100

    Solved problem 23

---
+++ 023/abundantNumbers.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+import os
+import math
+
+def nonAbundentNumberSum():
+    result = 0
+    limit = 28123
+    abundant = abundantNumbers(limit)
+    for i in range(0, limit):
+        if not isAbundentNumberSum(i, abundant):
+            result += i
+    return result
+
+def abundantNumbers(limit):
+    abundant = set()
+    for i in range(12, limit):
+        if i < sum(divisors(i)):
+            abundant.add(i)
+    return abundant
+
+def isAbundentNumberSum(number, abundant):
+    for i in abundant:
+        if i >= number:
+            return False
+        if number - i in abundant:
+            return True
+    return False
+
+def divisors(n):
+    divisors = set([1])
+    for i in range(2, int(math.sqrt(n) + 1)):
+        if n % i == 0:
+            divisors.add(i)
+            divisors.add(n/i) 
+    return divisors    
+
+
---
+++ 023/test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+import abundantNumbers 
+import unittest
+
+class TestAbundantNumbers (unittest.TestCase):
+    
+    def test_abundantNumbers(self):
+        result = len(abundantNumbers.abundantNumbers(100))
+        self.assertEqual(result, 21)
+        result = abundantNumbers.abundantNumbers(40)
+        self.assertEqual(sorted(result), [12, 18, 20, 24, 30, 36])  
+
+    def test_nonAbundentNumberSum(self):
+        result = abundantNumbers.nonAbundentNumberSum()
+        self.assertEqual(result, 4179871)
+        print "Result: {0}".format(result)
+        
+if __name__ == '__main__':
+    unittest.main()
