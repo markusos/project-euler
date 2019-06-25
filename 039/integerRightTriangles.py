f95997981fb4af793af615daaeeb6e38e4392d45
Markus Östberg <markus@ostberg.dev>
2019-06-25 19:31:39 +0200

    fix indentation

--- 039/integerRightTriangles.py
+++ 039/integerRightTriangles.py
@@ -3,33 +3,33 @@ import math
 
 # Test if a, b, c makes a Pythagorean triple
 def is_pythagorean_triple(a, b, c):
-	return (a * a + b * b == c * c)
+    return (a * a + b * b == c * c)
 
 # Generate all Pythagorean triples up to p_max
 def pythagorean_triples(p_max):
-	triples = {}
+    triples = {}
 
-	for a in range(1, int(p_max/2)):
-		for b in range(a + 1, int(p_max/2)):
-			c = int(math.sqrt(a*a + b*b))
-			p = a + b + c
-			if p not in triples:
-				triples[p] = []
-			if is_pythagorean_triple(a, b, c):
-				triples[p].append([a, b, c])
+    for a in range(1, int(p_max/2)):
+        for b in range(a + 1, int(p_max/2)):
+            c = int(math.sqrt(a*a + b*b))
+            p = a + b + c
+            if p not in triples:
+                triples[p] = []
+            if is_pythagorean_triple(a, b, c):
+                triples[p].append([a, b, c])
 
-	return triples
+    return triples
 
 def max_pythagorean_triples(p_max):
-	triples_max = 0
-	p_triples_max = 0
+    triples_max = 0
+    p_triples_max = 0
 
-	triples = pythagorean_triples(p_max)
+    triples = pythagorean_triples(p_max)
 
-	for p in range(1, p_max):
-		count = len(triples.get(p, []))
-		if count > triples_max:
-			triples_max = count
-			p_triples_max = p
+    for p in range(1, p_max):
+        count = len(triples.get(p, []))
+        if count > triples_max:
+            triples_max = count
+            p_triples_max = p
 
-	return p_triples_max
+    return p_triples_max
